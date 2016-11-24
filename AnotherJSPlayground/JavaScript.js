122d4761f76da8741dfc3e143630a2a2eb07b39a
Brad Kilkenney <bkilkenney@gmail.com>
2016-11-24 11:15:31 -0500

    Add html and 2 scripts

---
+++ AnotherJSPlayground/JavaScript.js
@@ -0,0 +1,31 @@
+﻿//function getSize(width, height, depth) {
+//    var area = width * height;
+//    var volume = width * height * depth;
+//    var sizes = [area, volume];
+//    return sizes;
+//}
+
+//var areaOne = getSize(3, 2, 3)[0];
+//var VolumeOne = getsize(3, 2, 3)[1];
+
+//document.write(areaOne);
+//document.write(VolumeOne);
+
+//var area = function (width, height) {
+//    return width * height;
+//}
+
+//var size = area(3, 4);
+
+//document.write(size);
+
+
+//Immediately Invoked Function Expressions("Iffy")
+
+//var area = (function () {
+//    var width = 3;
+//    var height = 2;
+//    return width * height;
+//})();
+
+//document.write(area);
---
+++ AnotherJSPlayground/Objects.js
@@ -0,0 +1,17 @@
+﻿var hotel = {
+    name: 'Homewood Suites',
+    rooms: 40,
+    booked: 25,
+    gym: true,
+    roomTypes: ['twin', 'double', 'suite'],
+
+    checkAvailability: function () {
+        return this.rooms - this.booked;
+    }
+};
+
+var hotelName = hotel.name;
+var roomsFree = hotel.checkAvailability();
+
+hotel.name = "Hampton Inn";
+document.write(hotel.name);
